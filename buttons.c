9c668796fcc8904b5ef2aee13160c73dbd0b31ad
GBenG <sps.tech.way@gmail.com>
2024-02-20 13:41:11 +0200

    longpress draft

--- buttons.c
+++ buttons.c
@@ -17,11 +17,11 @@ struct button_s btn3 = { .gpio = BUTTON3_PIN };
 struct button_s btn4 = { .gpio = BUTTON4_PIN };
 
 
-bool BTN_Processing( struct button_s *btn ){
+enum btn_press_t BTN_Processing( struct button_s *btn ){
   //Debounce
   if( btn->debounce != 0 ){
     btn->debounce--;
-    return false;
+    return BTN_NO_PRESS;
   }
   //Processing
   btn->press = (bool)!digitalRead(btn->gpio);
@@ -29,18 +29,27 @@ bool BTN_Processing( struct button_s *btn ){
     btn->prev_state = btn->press;
     if( btn->press ){
       LED_DebugBlink(false);
-      return true;
+      btn->hold  = true;
+      btn->timer = 300;
+      return BTN_SHORT_PRESS;
     } else {
-      Keyboard_releaseAll();
       LED_DebugBlink(true);
     }
     btn->debounce = BTN_DEBOUNCE_TIMEOUT;
+  }else{
+    if( btn->hold == true && btn->timer != 0 ){
+      btn->timer--;
+      if( btn->timer == 0){
+        btn->hold = false;
+        return BTN_LONG_PRESS;
+      }
+    }
   }
-  return false;
+  return BTN_NO_PRESS;
 }
 
 void BTN_Main( void ){
-  if( BTN_Processing( &btn4 ) == true ){
+  if( BTN_Processing( &btn4 ) == BTN_SHORT_PRESS ){
     //Sequence 'ä'
     Keyboard_press(KEY_LEFT_ALT);
     Keyboard_click(KEYPAD_0);
@@ -48,8 +57,10 @@ void BTN_Main( void ){
     Keyboard_click(KEYPAD_2);
     Keyboard_click(KEYPAD_8);
     Keyboard_release(KEY_LEFT_ALT);
+    delay(100);
+    Keyboard_releaseAll();
   }
-  if( BTN_Processing( &btn3 ) == true ){
+  if( BTN_Processing( &btn3 ) == BTN_SHORT_PRESS ){
     //Sequence 'ö'
     Keyboard_press(KEY_LEFT_ALT);
     Keyboard_click(KEYPAD_0);
@@ -57,8 +68,10 @@ void BTN_Main( void ){
     Keyboard_click(KEYPAD_4);
     Keyboard_click(KEYPAD_6);
     Keyboard_release(KEY_LEFT_ALT);
+    delay(100);
+    Keyboard_releaseAll();
   }
-  if( BTN_Processing( &btn2 ) == true ){
+  if( BTN_Processing( &btn2 ) == BTN_SHORT_PRESS ){
     //Sequence 'ü'
     Keyboard_press(KEY_LEFT_ALT);
     Keyboard_click(KEYPAD_0);
@@ -66,8 +79,10 @@ void BTN_Main( void ){
     Keyboard_click(KEYPAD_5);
     Keyboard_click(KEYPAD_2);
     Keyboard_release(KEY_LEFT_ALT);
+    delay(100);
+    Keyboard_releaseAll();
   }
-  if( BTN_Processing( &btn1 ) == true ){
+  if( BTN_Processing( &btn1 ) == BTN_SHORT_PRESS ){
     //Sequence 'ß'
     Keyboard_press(KEY_LEFT_ALT);
     Keyboard_click(KEYPAD_0);
@@ -75,5 +90,7 @@ void BTN_Main( void ){
     Keyboard_click(KEYPAD_2);
     Keyboard_click(KEYPAD_3);
     Keyboard_release(KEY_LEFT_ALT);
+    delay(100);
+    Keyboard_releaseAll();
   }
 }
